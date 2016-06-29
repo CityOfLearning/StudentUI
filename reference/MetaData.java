e39d1fab3ebd2f4faf901a2d455be56268ab29d3
Dominic Amato <dominic.a.amato@gmail.com>
2016-06-28 20:16:56 -0500

    url update

--- reference/MetaData.java
+++ reference/MetaData.java
@@ -9,7 +9,7 @@ public class MetaData {
 		metadata.modId = Reference.MOD_ID;
 		metadata.name = Reference.MOD_NAME;
 		metadata.description = "This creates the UI's for students";
-		metadata.url = "https://github.com/Digital-Youth-Network/StudentUI";
+		metadata.url = "https://github.com/CityOfLearning/StudentUI";
 		metadata.version = Reference.VERSION;
 		metadata.authorList = Arrays.asList("Dom Amato");
 		metadata.credits = "";
