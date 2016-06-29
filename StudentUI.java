8119bd7e4b5ff5c5fff0bad7f7ace1723fdc2ff8
Dominic Amato <dominic.a.amato@gmail.com>
2016-06-29 13:03:01 -0500

    frozen player on server side too

--- StudentUI.java
+++ StudentUI.java
@@ -28,9 +28,6 @@ public class StudentUI {
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static Proxy proxy;
 
-	@Mod.Metadata(Reference.MOD_ID)
-	public ModMetadata metadata;
-
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 
@@ -39,6 +36,6 @@ public class StudentUI {
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		metadata = MetaData.init(metadata);
+		MetaData.init(event.getModMetadata());
 	}
 }
--- proxy/Server.java
+++ proxy/Server.java
@@ -1,10 +1,20 @@
 package com.dyn.student.proxy;
 
+import com.dyn.DYNServerMod;
+import com.dyn.student.StudentUI;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 public class Server implements Proxy {
 
 	@Override
 	public void init() {
-
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	/**
@@ -14,6 +24,15 @@ public class Server implements Proxy {
 	public void renderGUI() {
 		// Actions on render GUI for the server (logging)
 
+	}
+	
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onPlayerUpdate(LivingEvent.LivingUpdateEvent event) {
+		if (event.entity instanceof EntityPlayer) {
+			if (DYNServerMod.frozenPlayers.contains(event.entity.getName())) {
+					event.setCanceled(true);
+			}
+		}
 	}
 
 }
