9050ea543a3564e17099d59447ac77da6f7fedd8
Dominic Amato <dominic.a.amato@gmail.com>
2017-05-12 00:37:04 -0500

    start api integration

--- proxy/Server.java
+++ proxy/Server.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.Map;
 
 import com.dyn.DYNServerMod;
+import com.forgeessentials.api.APIRegistry;
+import com.forgeessentials.api.permissions.FEPermissions;
+import com.forgeessentials.api.permissions.IPermissionsHelper;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,6 +14,9 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class Server implements Proxy {
+
+	public static final String DYN_PERM = FEPermissions.PLAYER + ".dyn";
+	public static final String STUDENT_PERM = DYN_PERM + ".student";
 
 	@Override
 	public Map<String, ?> getKeyBindings() {
@@ -20,6 +26,21 @@ public class Server implements Proxy {
 	@Override
 	public void init() {
 		MinecraftForge.EVENT_BUS.register(this);
+
+		IPermissionsHelper perms = APIRegistry.perms;
+		perms.getServerZone().getRootZone();
+
+		perms.registerPermissionDescription(STUDENT_PERM, "DYN Student permissions");
+
+		// ways of handling permissions
+		// perms.registerPermission(PERM_COLUMN, PermissionLevel.TRUE,
+		// "If true, all plots will always extend from bottom to top of the
+		// world. Price will only depend on X and Z dimensions.");
+		// perms.registerPermissionProperty(PERM_SIZE_MIN, "3", "Minimum size of
+		// one plot axis");
+		// perms.registerPermissionDescription(PERM_SIZE_MAX, "Maximum size of
+		// one plot axis");
+
 	}
 
 	@SubscribeEvent
