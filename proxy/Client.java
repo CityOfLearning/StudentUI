3a7f37499fbbe795cbc93a6226b4082f35b3d7f4
Dominic Amato <dominic.a.amato@gmail.com>
2017-06-29 17:20:59 -0500

    clean up code

--- proxy/Client.java
+++ proxy/Client.java
@@ -6,16 +6,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.lwjgl.input.Keyboard;
-
 import com.dyn.DYNServerMod;
 import com.dyn.server.network.NetworkManager;
 import com.dyn.server.network.packets.server.ServerCommandMessage;
 import com.dyn.student.StudentUI;
-import com.dyn.student.gui.Home;
 import com.dyn.utils.BooleanChangeListener;
 import com.dyn.utils.PlayerAccessLevel;
-import com.rabbit.gui.RabbitGui;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
@@ -24,18 +20,15 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 public class Client implements Proxy {
 
-	private KeyBinding homeKey;
-
 	@Override
 	public Map<String, ?> getKeyBindings() {
 		Map<String, KeyBinding> keys = new HashMap();
-//		keys.put("student", homeKey);
+		// keys.put("student", homeKey);
 		return keys;
 	}
 
@@ -43,8 +36,9 @@ public class Client implements Proxy {
 	public void init() {
 		if ((DYNServerMod.accessLevel == PlayerAccessLevel.STUDENT)) {
 			MinecraftForge.EVENT_BUS.register(this);
-//			homeKey = new KeyBinding("key.toggle.studentui", Keyboard.KEY_M, "key.categories.toggle");
-//			ClientRegistry.registerKeyBinding(homeKey);
+			// homeKey = new KeyBinding("key.toggle.studentui", Keyboard.KEY_M,
+			// "key.categories.toggle");
+			// ClientRegistry.registerKeyBinding(homeKey);
 
 			BooleanChangeListener listener = (event, show) -> {
 				if (event.getDispatcher().getFlag()) {
@@ -72,9 +66,10 @@ public class Client implements Proxy {
 		if ((Minecraft.getMinecraft().currentScreen instanceof GuiChat)) {
 			return;
 		}
-//		if ((DYNServerMod.accessLevel == PlayerAccessLevel.STUDENT) && homeKey.isPressed()) {
-//			RabbitGui.proxy.display(new Home());
-//		}
+		// if ((DYNServerMod.accessLevel == PlayerAccessLevel.STUDENT) &&
+		// homeKey.isPressed()) {
+		// RabbitGui.proxy.display(new Home());
+		// }
 	}
 
 	@SubscribeEvent
